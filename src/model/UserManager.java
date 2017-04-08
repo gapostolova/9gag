c3d5812e732f365949b6528c0373c2f9fb8eb851
jevgenieva <jevgenieva@gmail.com>
2017-04-08 12:42:27 +0300

    user manager - in progress

--- src/model/UserManager.java
+++ src/model/UserManager.java
@@ -1,5 +1,22 @@
 package model;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 public class UserManager {
 
+	public static UserManager instance;
+	private ConcurrentHashMap<String, User> temp = new ConcurrentHashMap<>();
+	//temp collection for testing
+	
+	private UserManager() {}
+	
+	public static synchronized UserManager getInstance() {
+		if(instance == null)
+			instance = new UserManager();
+		return instance;
+	}
+	
+	public boolean validateLogin(String username, String password) {
+		return UserDAO.allUsers.get(username).getPassword().equals(password);	
+	}
 }
